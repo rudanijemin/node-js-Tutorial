97c2f0ccf35d572838f25d57d1dd028b51a171a6
jemin <rudanijemin18@gmail.com>
2021-12-24 11:33:41 +0530

    fs Async

---
+++ fsAsync/home.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+//addfile Asyncs
+//fs.writeFile("fsAsync/read.txt","to day is awesome",(err) => {
+  //console.log("file compplated");
+    //console.log(err);
+//});
+
+//append
+//fs.appendFile("fsAsync/read.txt","today i am learn node js",(err)=>{
+  //  console.log(err);
+//});
+
+//readfile
+fs.readFile("fsAsync/read.txt","utf-8",(err,data)=>{
+    console.log(data);
+});
---
+++ middle.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+//fs.mkdirSync("new");
+//fs.readFileSync("new/bio.txt","utf-8");
+//fs.rmdirSync("new");
