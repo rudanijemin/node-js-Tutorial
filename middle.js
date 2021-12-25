1dd80391aaa31a33c38cbf0d726e291d4b0fb18d
jemin <rudanijemin18@gmail.com>
2021-12-25 10:25:52 +0530

    Async and Sync

--- middle.js
+++ middle.js
@@ -1,4 +1,15 @@
+//Sync vs Async
+
 const fs = require("fs");
-//fs.mkdirSync("new");
-//fs.readFileSync("new/bio.txt","utf-8");
-//fs.rmdirSync("new");
+//fs.writeFileSync("read.txt","ode js is a supper language");
+
+//Sync
+//const data = fs.readFileSync("read.txt","utf-8");
+//console.log(data);
+//console.log("hello ");
+
+//Async
+ fs.readFile("read.txt","utf-8",(err,data)=>{
+     console.log(data);
+});
+console.log("hello");
