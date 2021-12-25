21abcd21e5405a60d1abd37ee7cf8604659a4215
jemin <rudanijemin18@gmail.com>
2021-12-25 11:05:08 +0530

    Async challange

---
+++ fsAsync/nodech.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+//fs.mkdir("challange",(err)=>{
+ //   console.log("folder is create");
+//});
+
+//fs.writeFile("challange/bio.txt","th day is nice day",(err)=>{
+ //   console.log("file is create");
+//});
+
+//fs.readFile("challange/bio.txt","utf-8",(err,data)=>{
+  //  console.log(data);
+//});
+//console.log("hello");
+
+//fs.unlink("challange/bio.txt",()=>{
+//});
+
+fs.rmdir("challange",()=>{});
