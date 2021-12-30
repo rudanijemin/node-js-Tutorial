f4ea8385e9da31c08e5ec97f7e9af56563a90616
jemin <rudanijemin18@gmail.com>
2021-12-30 15:35:37 +0530

    html request

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,9 +1,20 @@
 const http = require("http");
-
 const server = http.createServer((req,res)=>{
-    res.end("hello from the  other new side");
+    //console.log(req.url);
+    if(req.url == "/"){
+        res.end("hello from home page");
+    }
+    else if(req.url == "/about"){
+        res.end("hello from about page");
+    }
+    else if(req.url == "/contact"){
+        res.end("hello from contact page");
+    }
+    else {
+        res.writeHead(404,{"content-type" : "text/html"});
+        res.end("404 error page does not exit");
+    }
 });
-
-server.listen(8000 , "127.0.0.1",()=>{
-    console.log("listing to port no 8000");
+server.listen(7000 , "127.0.0.1",()=>{
+    console.log("listing to port no 7000");
 });
