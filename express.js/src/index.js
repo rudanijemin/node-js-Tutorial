d2cba7e6821c6ba1d4aedadbbaae397fafff3299
jemin <rudanijemin18@gmail.com>
2022-03-05 17:40:05 +0530

    templates engines

--- express.js/src/index.js
+++ express.js/src/index.js
@@ -5,16 +5,26 @@ const app = express();
 
 
 const staticPath = path.join(__dirname,"../public");
+const templatePath = path.join(__dirname,"../templates");
+
+
+//set view engine
+app.set("view engine","hbs");
+app.set("views",templatePath);
 
 //bulitin middleware
-app.use(express.static(staticPath));
+// app.use(express.static(staticPath));
 
+app.get("",(req,res)=>{
+    res.render("index");
+});
 app.get("/",(req,res)=>{
     res.send("hello word .. ");
 });
 app.get("/about",(req,res)=>{
     res.send("about page .. ");
 });
+
 
 app.listen(8000,()=>{
     console.log("listing the port 8000")
