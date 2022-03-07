d760d1a455d321cf3f0fbf98ef7997c555aafdb7
jemin <rudanijemin18@gmail.com>
2022-03-07 18:39:35 +0530

    add footer

--- express.js/src/index.js
+++ express.js/src/index.js
@@ -1,16 +1,18 @@
 const path = require("path");
 const express = require("express");
-
 const app = express();
-
+const hbs =require("hbs");
 
 const staticPath = path.join(__dirname,"../public");
-const templatePath = path.join(__dirname,"../templates");
+const templatePath = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname,"../templates/partials");
 
 
 //set view engine
 app.set("view engine","hbs");
 app.set("views",templatePath);
+
+hbs.registerPartials(partialsPath); 
 
 //bulitin middleware
 // app.use(express.static(staticPath));
@@ -18,13 +20,27 @@ app.set("views",templatePath);
 app.get("",(req,res)=>{
     res.render("index");
 });
+
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
 app.get("/",(req,res)=>{
-    res.send("hello word .. ");
+    res.send("hello word .. ",{
+        chanelName: "je",
+    });
+    
 });
 app.get("/about",(req,res)=>{
     res.send("about page .. ");
 });
 
+app.get("*",(req,res)=>{
+    res.render("404",{
+        errorComent: "Opps page could not be fount",
+
+    })
+
+})
 
 app.listen(8000,()=>{
     console.log("listing the port 8000")
